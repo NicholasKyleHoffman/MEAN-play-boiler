1579192f31646c35b9084d2848afdebeb54c1256
Nick <hoffman.nick10@gmail.com>
2015-10-05 21:43:54 -0700

    missing bin and models

---
+++ models/Comments.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.Schema({    // schema for formatting how comment data is stored
+  body: String,
+  author: String,
+  upvotes: {type: Number, default: 0},
+  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+});
+
+mongoose.model('Comment', CommentSchema);
